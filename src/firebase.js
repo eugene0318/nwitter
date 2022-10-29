3170ee65af71582c78e06046bb793549cd844eee
eugene0318 <nttott@gmail.com>
2022-10-29 23:43:59 +0900

    수정1

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHD8wavK4nFaWP3L0Kl9pUuXyLH6Wgxkg",
+  authDomain: "nwitter-76c90.firebaseapp.com",
+  projectId: "nwitter-76c90",
+  storageBucket: "nwitter-76c90.appspot.com",
+  messagingSenderId: "973135324497",
+  appId: "1:973135324497:web:e3391bee5b41433ffe9f70",
+};
+
+export default firebase.initializeApp(firebaseConfig);
