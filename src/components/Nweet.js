32983cfc4dfeec00b03037d9aec49a37e5d42282
eugene0318 <nttott@gmail.com>
2022-11-04 02:12:49 +0900

    nweet edit & update

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../fbase";
 
 const Nweet = ({ nweetObj, isOwner }) => {
-  const onDeleteClick = () => {
-    const ok = confirm("are you sure you wanna to delete this nweet");
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("are you sure you wanna to delete this nweet");
+    if (ok) {
+      console.log(`id : + ${nweetObj.id}`);
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+  const toggelEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    console.log(nweetObj, newNweet);
+    await dbService.doc(`nweets/${nweetObj.id}`).update({
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewNweet(value);
   };
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Delete Nweet</button>
-          <button>Edit Nweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="edit your nweet"
+              defaultValue={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggelEditing}>Cancel</button>
         </>
-      )}{" "}
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggelEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 };
